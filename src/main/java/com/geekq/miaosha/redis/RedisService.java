36efb95872440931013eb8b1c361f49fc40fc0eb
laihuan <laihuan@163.com>
2020-01-20 16:05:03 +0800

    RedisService中，将方法进行模板化

--- src/main/java/com/geekq/miaosha/redis/RedisService.java
+++ src/main/java/com/geekq/miaosha/redis/RedisService.java
@@ -1,11 +1,13 @@
 package com.geekq.miaosha.redis;
 
-import com.alibaba.fastjson.JSON;
 import com.geekq.miaosha.utils.BeanUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import redis.clients.jedis.*;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.ScanParams;
+import redis.clients.jedis.ScanResult;
 
 import java.util.ArrayList;
 import java.util.List;
